3d9763dff2c77d75f948be53775d6db5cf6b4f72
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-06-12 20:30:14 +0300

    Update to 1.21 (rc 1)
    Oh god this mixin is so cursed please don't send me to hell

--- src/main/java/top/offsetmonkey538/hcwt/mixin/HoneyBottleItemMixin.java
+++ src/main/java/top/offsetmonkey538/hcwt/mixin/HoneyBottleItemMixin.java
@@ -1,26 +1,48 @@
 package top.offsetmonkey538.hcwt.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.HoneyBottleItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
+import net.minecraft.registry.entry.RegistryEntry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(HoneyBottleItem.class)
 public abstract class HoneyBottleItemMixin {
 
-    @Inject(
+    @WrapOperation(
+            require = 0,
+            method = "finishUsing",
+            at = @At(
+                    remap = false,
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/class_1309;method_6016(Lnet/minecraft/class_1291;)Z"
+            )
+    )
+    private boolean hcwt$oneTwenty$makeHoneyRemoveWitheringEffect(LivingEntity instance, StatusEffect type, Operation<Boolean> original) {
+        // This is so cursed but for some reason I really want there to only be one jar file that supports 1.20 to 1.21
+        try {
+            original.call(instance, (StatusEffect) StatusEffects.class.getField(FabricLoader.getInstance().getMappingResolver().mapFieldName("intermediary", "net.minecraft.class_1294", "field_5920", "Z")).get(null));
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+        return original.call(instance, type);
+    }
+
+    @WrapOperation(
+            require = 0,
             method = "finishUsing",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/entity/LivingEntity;removeStatusEffect(Lnet/minecraft/entity/effect/StatusEffect;)Z"
+                    target = "Lnet/minecraft/entity/LivingEntity;removeStatusEffect(Lnet/minecraft/registry/entry/RegistryEntry;)Z"
             )
     )
-    private void hcwt$makeHoneyRemoveWitheringEffect(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir) {
-        user.removeStatusEffect(StatusEffects.WITHER);
+    private boolean hcwt$oneTwentyOne$makeHoneyRemoveWitheringEffect(LivingEntity instance, RegistryEntry<StatusEffect> type, Operation<Boolean> original) {
+        original.call(instance, StatusEffects.WITHER);
+        return original.call(instance, type);
     }
 }
